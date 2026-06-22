768625aec0aa64d01b8f935bfbf8f804e0e5ee5f
farhanjameel894 <farhanjameel894@gmail.com>
2026-06-22 19:11:03 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail ="fari@gmail.com"
+var accountPassword ="abc@123"
+accountCity ="kashmir"
+let acountState;
+console.log(accountId);
+
+accountEmail ="fano@gmail.com"
+accountPassword ="dsa@321"
+accountCity ="islamabad"
+
+console.table([accountEmail,accountPassword,accountCity, accountState]);
